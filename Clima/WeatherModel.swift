05da0f72371d15a255a4a69ef0d954dd4fa0f62b
Abdelrahman Nasser <abdonasser1810@gmail.com>
2020-09-13 01:06:13 +0200

    Upload Project

---
+++ Clima/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by 3bdo on 6/5/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/weatherData.swift
@@ -0,0 +1,9 @@
+//
+//  weatherData.swift
+//  Clima
+//
+//  Created by 3bdo on 6/4/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
